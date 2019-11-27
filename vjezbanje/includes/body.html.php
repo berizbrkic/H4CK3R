c023cece59c6a8da75c7bb815234385122db4617
berizbrkic <57287208+berizbrkic@users.noreply.github.com>
2019-11-27 22:03:35 +0100

    Add files via upload

---
+++ vjezbanje/includes/body.html.php
@@ -0,0 +1,28 @@
+<!-- Header; Start -->
+<?php require 'header.html.php'; ?>
+<!--Header; End-->
+<!-- Footer; Start -->
+<?php require 'footer.html.php'; ?>
+<!--Footer; End -->
+<body data-spy="scroll" data-target=".fixed-top">
+    
+    <!-- Preloader; Start -->
+<?php require 'preloader.html.php';	?>
+    <!-- end of preloader -->
+    
+
+    <!-- Navigation; Start  -->
+    <?php require 'nav.html.php'; ?>
+    <!-- end of navbar -->
+    <!-- end of navigation -->
+
+    <!-- Copyright; Start -->
+    <?php require 'copyright.html.php'; ?>
+     
+    <!-- end of copyright -->
+    
+    	
+    <!-- Scripts; Start -->
+    <?php require 'scripts.html.php'; ?>
+    <!--Scripts; End -->
+</body>
---
+++ vjezbanje/web/index.html.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!--Head; Start -->
+    <?php require 'includes/head.html.php'; ?>
+    <!--Head; End  -->
+    <!--Body; Start -->
+    <?php require 'includes/body.html.php'; ?>
+    <!--Body; End -->
+
+</html>
